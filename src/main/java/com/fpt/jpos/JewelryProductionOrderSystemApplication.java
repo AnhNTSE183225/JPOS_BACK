f2f7ff9c3a8f81377f6b5279fdfd998dd2a440ae
nctbinh <binhnctse183284@fpt.edu.vn>
2024-05-27 10:08:07 +0700

    Initial commit

---
+++ src/main/java/com/fpt/jpos/JewelryProductionOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.jpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewelryProductionOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JewelryProductionOrderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpt/jpos/JewelryProductionOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.jpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelryProductionOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
