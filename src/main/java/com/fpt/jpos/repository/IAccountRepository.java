bb37351383a5a596224aec02ba4938bb61a62be6
nctbinh <binhnctse183284@fpt.edu.vn>
2024-05-28 10:21:19 +0700

    Add findByUsername method

--- src/main/java/com/fpt/jpos/repository/IAccountRepository.java
+++ src/main/java/com/fpt/jpos/repository/IAccountRepository.java
@@ -3,5 +3,10 @@ package com.fpt.jpos.repository;
 import com.fpt.jpos.pojo.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IAccountRepository extends JpaRepository<Account, String> {
+
+    Optional<Account> findByUsername(String username);
+
 }
