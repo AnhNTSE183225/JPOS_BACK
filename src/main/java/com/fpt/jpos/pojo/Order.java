0d2e9605557b7871585811534c0f62ddb78fe770
VoPhuDat <datvpse171622@fpt.edu.vn>
2024-05-28 09:26:44 +0700

    Add "Receive customer request" function

--- src/main/java/com/fpt/jpos/pojo/Order.java
+++ src/main/java/com/fpt/jpos/pojo/Order.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @Entity
 @Table(name = "[Order]")
 @Data
-@NoArgsConstructor  // using lombok
+@NoArgsConstructor  // using Lombok
 @AllArgsConstructor
 public class Order {
 
