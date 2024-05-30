6b99b0c22e1cf07c8e409c007bbc6c38e8359109
nctbinh <binhnctse183284@fpt.edu.vn>
2024-05-30 20:39:34 +0700

    Add findAllByStatusAndStaff() method

--- src/main/java/com/fpt/jpos/repository/IOrderRepository.java
+++ src/main/java/com/fpt/jpos/repository/IOrderRepository.java
@@ -3,13 +3,18 @@ package com.fpt.jpos.repository;
 import com.fpt.jpos.pojo.Customer;
 import com.fpt.jpos.pojo.Order;
 
+import java.util.List;
 import java.util.Optional;
 
+import com.fpt.jpos.pojo.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IOrderRepository extends JpaRepository<Order, Integer> {
-	Optional<Order> findByCustomer(Order customer);
+    Optional<Order> findByCustomer(Order customer);
+
+    @Query(value = "SELECT * FROM [Order] WHERE status != 'Completed' and (sale_staff_id = ?1 or sale_staff_id is null)", nativeQuery = true)
+    List<Order> findAllByStatusAndStaff(int saleStaffId);
 }
