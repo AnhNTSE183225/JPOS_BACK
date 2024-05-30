d79e1acc1dabace163967c7ad422f57ff536ef52
nctbinh <binhnctse183284@fpt.edu.vn>
2024-05-30 20:39:51 +0700

    Add getOrdersForSaleStaffs() method

--- src/main/java/com/fpt/jpos/service/IOrderService.java
+++ src/main/java/com/fpt/jpos/service/IOrderService.java
@@ -3,11 +3,15 @@ package com.fpt.jpos.service;
 import com.fpt.jpos.pojo.CustomerRequest;
 import com.fpt.jpos.pojo.Order;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface IOrderService {
 
     Order insertOrder(CustomerRequest customerRequest);
     //Order insertOrder(Order theOrder, int customerId);
 
-	String handleManagerResponse(Long id, boolean managerApproval);
+    String handleManagerResponse(int id, boolean managerApproval);
 
+    List<Order> getOrdersForSaleStaffs(int saleStaffId);
 }
