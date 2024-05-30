e1d31b60534edcc43e96cc67e1db4997e7b4fe5f
nctbinh <binhnctse183284@fpt.edu.vn>
2024-05-30 21:51:33 +0700

    Minor changes

---
+++ src/main/java/com/fpt/jpos/pojo/Material.java
@@ -0,0 +1,25 @@
+package com.fpt.jpos.pojo;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Material")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Material {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "material_id")
+    private int materialId;
+
+    @Column(name = "material_name")
+    private String materialName;
+
+}
