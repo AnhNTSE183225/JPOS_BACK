a3b305a226a4e1c31cb07f2be09cfad717e67885
BinhNCTSE183284 <binhnctse183284@fpt.edu.vn>
2024-05-31 15:03:40 +0700

    Add confirmDeposit() and acceptDesign() method

--- src/main/java/com/fpt/jpos/controller/OrderController.java
+++ src/main/java/com/fpt/jpos/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.fpt.jpos.controller;
 
 import com.fpt.jpos.pojo.CustomerRequest;
 import com.fpt.jpos.pojo.Order;
+import com.fpt.jpos.pojo.enums.OrderStatus;
 import com.fpt.jpos.service.IFileUploadService;
 import com.fpt.jpos.service.IOrderService;
 import jakarta.persistence.Table;
@@ -71,6 +72,17 @@ public class OrderController {
         }
     }
 
+    // Update order status to designing after confirming deposit
+    @PutMapping("/sales/orders/{id}/confirm-deposit")
+    @Transactional
+    public ResponseEntity<?> confirmDeposit(@PathVariable int id) {
+        Order order = orderService.updateOrderStatus(id, OrderStatus.designing);
+        if (order == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Order not found");
+        }
+        return ResponseEntity.ok(order);
+
+    }
 
     // Upload file by design staff
     @CrossOrigin
@@ -78,12 +90,24 @@ public class OrderController {
     @Transactional
     public ResponseEntity<?> uploadDesign(@RequestParam("file") MultipartFile file, @PathVariable Integer id) throws IOException {
 
-        String imageURL = fileUploadService.uploadFile(file, id);
+        String imageURL = fileUploadService.uploadModelDesignFile(file, id);
 
         if (imageURL == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Image is not found");
         } else {
             return ResponseEntity.ok(imageURL);
         }
+    }
+
+    // Customer accept design
+    @PostMapping("/customers/acceptDesign/{orderId}")
+    @Transactional
+    public ResponseEntity<?> acceptDesign(@PathVariable Integer orderId) {
+
+        Order theOrder = orderService.updateOrderStatus(orderId, OrderStatus.production);
+        if (theOrder == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Order not found");
+        }
+        return ResponseEntity.ok(theOrder);
     }
 }
