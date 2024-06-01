86b7bb6bae9a1d7d972aae3b73e2ed38ef76ed4c
BinhNCTSE183284 <binhnctse183284@fpt.edu.vn>
2024-06-01 22:27:31 +0700

    refactor: split updateOrderStatus to two method

--- src/main/java/com/fpt/jpos/service/IOrderService.java
+++ src/main/java/com/fpt/jpos/service/IOrderService.java
@@ -3,6 +3,7 @@ package com.fpt.jpos.service;
 import com.fpt.jpos.pojo.CustomerRequest;
 import com.fpt.jpos.pojo.Order;
 import com.fpt.jpos.pojo.enums.OrderStatus;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,11 +18,20 @@ public interface IOrderService {
 
     Order findById(Integer id);
 
-    Order updateOrderStatus(int orderId, OrderStatus status);
+    Order updateOrderStatusDesigning(int id);
+
+    Order updateOrderStatusProduction(int id);
 
     OrderStatus forwardQuotation(Integer id);
-    
+
     String retrieveQuotationFromStaff(Integer id);
 
     Order acceptOrder(Integer id);
+
+    //TODO
+    // - Production Staff set status to production_completed
+    // - Sale Staff deliver product to Customer - set status to Delivered
+    // - Sale Staff confirm final payment - set status to completed
+    // - Get productDesign list for customer to choose
+    // - Get diamond list for customer to choose
 }
