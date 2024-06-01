a2a2e45f6138c468e8d81ef57c0cc319ec297bbc
BinhNCTSE183284 <binhnctse183284@fpt.edu.vn>
2024-06-01 22:54:23 +0700

    feat: Add Payment service

---
+++ src/main/java/com/fpt/jpos/service/PaymentService.java
@@ -0,0 +1,9 @@
+package com.fpt.jpos.service;
+
+import com.fpt.jpos.pojo.Payment;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PaymentService implements IPaymentService {
+}
