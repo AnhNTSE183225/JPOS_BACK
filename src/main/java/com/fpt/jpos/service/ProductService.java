0a870a30e7bbbb4af5141f7d2e4b187af19a62e1
BinhNCTSE183284 <binhnctse183284@fpt.edu.vn>
2024-06-02 16:50:21 +0700

    refactor: Using ModelMapper library

--- src/main/java/com/fpt/jpos/service/ProductService.java
+++ src/main/java/com/fpt/jpos/service/ProductService.java
@@ -5,6 +5,8 @@ import com.fpt.jpos.repository.IDiamondRepository;
 import com.fpt.jpos.repository.IMaterialRepository;
 import com.fpt.jpos.repository.IProductMaterialRepository;
 import com.fpt.jpos.repository.IProductRepository;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,9 @@ public class ProductService implements IProductService {
     private final IProductMaterialRepository productMaterialRepository;
     private final IDiamondRepository diamondRepository;
 
+    ModelMapper modelMapper = new ModelMapper();
+
+
     @Autowired
     public ProductService(IProductRepository productRepository, IMaterialRepository materialRepository, IProductMaterialRepository productMaterialRepository, IDiamondRepository diamondRepository) {
         this.productRepository = productRepository;
@@ -29,20 +34,17 @@ public class ProductService implements IProductService {
     }
 
     @Override
+    @Transactional
     public Product saveProduct(ProductDTO productDTO) {
-        Product product = new Product();
-        product.setProductName(productDTO.getProductName());
-        product.setProductType(productDTO.getProductType());
-        product.setEDiamondPrice(productDTO.getEMaterialPrice());
-        product.setEMaterialPrice(productDTO.getEDiamondPrice());
-        product.setProductionPrice(productDTO.getProductionPrice());
+
+        Product product = modelMapper.map(productDTO, Product.class);
 
         product = productRepository.save(product);
 
-        for(Pair<Integer, Double> materialPair : productDTO.getMaterialsIds()) {
+        for (Pair<Integer, Double> materialPair : productDTO.getMaterialsIds()) {
             Material material = materialRepository.findById(materialPair.getFirst()).orElseThrow(() -> new RuntimeException("Material not found"));
 
-            ProductMaterialId productMaterialId = new ProductMaterialId(product.getProductId(),material.getMaterialId());
+            ProductMaterialId productMaterialId = new ProductMaterialId(product.getProductId(), material.getMaterialId());
 
             ProductMaterial productMaterial = new ProductMaterial();
             productMaterial.setId(productMaterialId);
@@ -54,7 +56,7 @@ public class ProductService implements IProductService {
         }
 
         List<Diamond> diamonds = new ArrayList<>();
-        for(Integer id : productDTO.getDiamondIds()) {
+        for (Integer id : productDTO.getDiamondIds()) {
             Diamond diamond = diamondRepository.findById(id).orElse(null);
             if (diamond != null) {
                 diamonds.add(diamond);
