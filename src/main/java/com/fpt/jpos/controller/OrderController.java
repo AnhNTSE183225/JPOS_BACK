40b3f14ae4c9ff81d5f6d6de7072c81621c3db80
BinhNCTSE183284 <binhnctse183284@fpt.edu.vn>
2024-06-02 18:06:50 +0700

    refactor: remove unused import

--- src/main/java/com/fpt/jpos/controller/OrderController.java
+++ src/main/java/com/fpt/jpos/controller/OrderController.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.service.annotation.GetExchange;
 
 import java.io.IOException;
 import java.util.List;
