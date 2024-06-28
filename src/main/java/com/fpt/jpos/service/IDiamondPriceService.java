a8794d041b52ac1256349ce51b12c05e19ede7e3
BinhNCTSE183284 <binhnctse183284@fpt.edu.vn>
2024-06-28 17:00:08 +0700

    feat: CRUD for MaterialPriceList

--- src/main/java/com/fpt/jpos/service/IDiamondPriceService.java
+++ src/main/java/com/fpt/jpos/service/IDiamondPriceService.java
@@ -9,8 +9,12 @@ import java.util.List;
 public interface IDiamondPriceService {
 
     DiamondPrice addDiamondPrice(DiamondPrice diamondPrice);
+
     DiamondPrice updateDiamondPrice(Integer diamondPriceId, Double newPrice);
+
     void deletePrice(Integer diamondPriceId);
+
     Page<DiamondPrice> getAllDiamondPrice(int pageNo, int pageSize);
+
     Double getSingleDiamondPrice(DiamondPriceQueryDTO diamondPriceQueryDTO);
 }
