6d52ffd8d4daa18c6049c2cc4507bb4aebad159b
BinhNCTSE183284 <binhnctse183284@fpt.edu.vn>
2024-07-21 12:16:53 +0700

    refactor: clean - remove commented code

--- src/main/java/com/fpt/jpos/auth/google/CustomOAuth2UserService.java
+++ src/main/java/com/fpt/jpos/auth/google/CustomOAuth2UserService.java
@@ -10,13 +10,8 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 // Binh
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
-
-    private final CustomerService customerService;
-    private final UserService userService;
-    private final JwtService jwtService;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
--- src/main/java/com/fpt/jpos/auth/google/UserService.java
+++ src/main/java/com/fpt/jpos/auth/google/UserService.java
@@ -1,9 +1,7 @@
 package com.fpt.jpos.auth.google;
 
-import com.fpt.jpos.pojo.Account;
 import com.fpt.jpos.repository.IAccountRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,15 +9,6 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final IAccountRepository accountRepository;
-    private final PasswordEncoder passwordEncoder;
 
-
-    public Account getAccount(String email) {
-        return accountRepository.findOneByEmail(email);
-    }
-
-    public void saveAccount(Account account) {
-        accountRepository.save(account);
-    }
 
 }
